05026e0b76ca062fc8c141c97dd7c785506cef16
Aluma Gelbard <Aluma Gelbard>
2016-12-31 00:18:33 +0200

    synonym update

--- Synonym/synonym.py
+++ Synonym/synonym.py
@@ -4,22 +4,30 @@ __author__ = 'User'
 import nltk
 import string
 from nltk.corpus import wordnet as wn
+import operator
+from collections import Counter
+
 
 
 def main():
-    #print(synonym('spark.v.01').getSynonym())
-    for ss in wn.synsets('before'):
-        print(ss.name(), ss.lemma_names())
-
+    print(synonym('phone').getSynonym())
     pass
 
 
 class synonym(object):
     def getSynonym(self):
         """
-        :return: synonym of the object
+        :return: synonyms of the object
         """
-        return (wn.synset(self._word)).hypernyms()
+        return self.choseSynonym(wn.synsets(self._word))
+
+    def choseSynonym(self, ss):
+        count = []
+        for s in ss:
+            for word in s.lemma_names():
+                if word != self._word:
+                    count.append(word)
+        return Counter(count).most_common(1)[0][0]
 
     def __init__(self, word):
         """
