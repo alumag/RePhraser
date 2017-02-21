aa0557ba3ca3a0d8624d45831f6b646651407940
Aluma Gelbard <Aluma Gelbard>
2017-02-21 20:49:19 +0200

    Parse Sentence

--- Synonym/ParseSentence.py
+++ Synonym/ParseSentence.py
@@ -4,15 +4,38 @@ import re
 import nltk
 import synonym
 import sys
+import os
+from nltk import FreqDist
+from collections import Counter
+from nltk.corpus import stopwords
+
 __import__("synonym")
 
+def TF(text):
+    # this function return the TF grade of word in a document
+    tokens = nltk.tokenize.word_tokenize(text)
+    filtered = [w for w in tokens if not w in stopwords.words('english')]
+    fdist = FreqDist(tokens)
+    try:
+        return fdist.most_common(1)
+    except:
+        return None
 
 def change_sentence(sentence):
     text = nltk.tokenize.word_tokenize(sentence)
     new_sentence = ""
+
+    if TF(sentence) is not None:
+        common = TF(sentence)[0][0]
+
+
     for cur in nltk.tag.pos_tag(text):
         if (cur[1] == "RB" or cur[1] == "VBP" or cur[1] == "VB" or cur[1] == "CC" or cur[1] == "PRT"):
             synonym1 = synonym.synonym(cur[0]).getSynonym()
+            print synonym1
+            if '_' in synonym1:
+                synonym1 = synonym1.replace('_', ' ')
+            print ">> ",synonym1
             sentence.replace(cur[0], synonym1)
             new_sentence += synonym1 + " "
         else:
@@ -27,8 +50,10 @@ def change_text(originalFile, newFile):
         new = open(newFile, 'w')
         new_sentences = []
         for sentence in sentences:
-            print sentences
-            new_sentences.append(change_sentence(sentence))
+            print sentences, os.linesep
+            changeSent = change_sentence(sentence)
+            new_sentences.append(changeSent)
+            print (">> ", changeSent, os.linesep)
         new.writelines(new_sentences)
     new.close()
     f.close()
@@ -49,8 +74,11 @@ def main(argv):
     elif len(argv) == 3 and argv[1] == "-f":
         try:
             change_text(argv[2], "log.txt")
-        except:
+        except Exception as inst:
             print "Error"
+            print type(inst)
+            print inst.args
+            print inst
         finally:
             print
 
