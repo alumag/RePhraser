5d7e331e8047541f8500277fcdc6507b1a800886
Aluma Gelbard <Aluma Gelbard>
2017-04-04 18:38:18 +0300

    GUI IS COMPLETE!! (everything but graphic ahah :P)

--- Web/ReText/ReTextWebsite/generate.py
+++ Web/ReText/ReTextWebsite/generate.py
@@ -2,31 +2,28 @@
 import os
 import subprocess
 
-
+"""
+This class run the ParseSentence.py script according to the input of the client.
+This algorthm is awful but actually we dont care. GET OVER IT!!
+"""
 class generate(object):
 	def __init__(self, sentence):
 		self._sentence = sentence
 	
-	def create_page(self):
-		print(os.path.dirname(os.path.realpath(__file__)))
+	def create_page(self):	
+		run_str = "python ../../Run/ParseSentence.py -s \"{0}\" > out.txt"
+		run_str = run_str.replace("{0}", self._sentence)
 		file = open("C:\Users\Viole\Documents\Magshimim\Project\Web\ReText\ReTextWebsite\\templates\ReTextWebsite\\rephrase.html", 'r')
 		newfile = open("C:\Users\Viole\Documents\Magshimim\Project\Web\ReText\ReTextWebsite\\templates\ReTextWebsite\\newRephrase.html", "w")
-
-		try:
-			p = subprocess.Popen('python try.py', stdout=subprocess.PIPE)
-			#p = subprocess.Popen(['python','../../../Run/ParseSentence.py -s "'+self._sentence+'"'], stdout=subprocess.PIPE)
-			self._sentence = p.communicate()
-			#self._sentence = os.system('python ../../../Run/ParseSentence.py -s "'+self._sentence+'"')
-			#self._sentence = subprocess.check_output('python ../../../Run/ParseSentence.py -s "'+self._sentence+'"', shell=True)
-		except subprocess.CalledProcessError as e:
-			self._sentence = e.output
-		finally:
-			print("Yeah yeah no exception")
-		print("Sentence: " + str(self._sentence))
+		
+		os.system(run_str)
+		output = open("out.txt", 'r')
+		self._sentence = output.read()
 		
 		if(self._sentence == None):
-			self._sentence = "Sorry, error :("
+			self._sentence = "Sorry, there is some error :(</br>Please connect Aluma/Arad and tell them it happen. Maybe theyll care.."
 
 		newfile.write(file.read().replace("<ul id=\"msg\"> </ul>", str(self._sentence)))
 		newfile.close()
 		file.close()
+		output.close()
