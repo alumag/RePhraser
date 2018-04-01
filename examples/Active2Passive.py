0f9a4566170d05a44b5d2637ef92e91ee9bc50ef
Aluma Gelbard <violetaluma@hotmail.com>
2018-04-01 19:50:17 +0300

    refactor /en and add example to Active2Passive

--- examples/Active2Passive.py
+++ examples/Active2Passive.py
@@ -8,4 +8,6 @@ Change a sentence from active to passive
 sentence = "Obama ate an apple"
 new_sentence = Active2Passive.change_sentence(sentence=sentence)
 
-print("Example #1\n", sentence, "\n", new_sentence)
+print "Example #1"
+print sentence
+print new_sentence
