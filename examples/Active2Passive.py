0e3a940b41faa037d080e25a8fec5bab0f9e3939
Aluma Gelbard <violetaluma@hotmail.com>
2018-04-01 19:55:04 +0300

    refactor en: bugs

--- examples/Active2Passive.py
+++ examples/Active2Passive.py
@@ -1,4 +1,7 @@
 from RePhraser import Active2Passive
+"""
+This feature can run only with python 2.7
+"""
 
 """
 EXAMPLE #1
@@ -8,6 +11,6 @@ Change a sentence from active to passive
 sentence = "Obama ate an apple"
 new_sentence = Active2Passive.change_sentence(sentence=sentence)
 
-print "Example #1"
-print sentence
-print new_sentence
+print("Example #1")
+print(sentence)
+print(new_sentence)
